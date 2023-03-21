35f00fff52bdcd1090c5da1f6fde1b28e393b299
Kole Dallam <kolejasonD@gmail.com>
2023-03-20 21:19:42 -0600

    getcity function

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,29 @@ $(document).ready(() => {
   });
 });
 
-
+function getCity(cityName) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&&units=imperial`;
+    const fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&&units=imperial`;
+  
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const date = dayjs().format("YYYY-MM-DD");
+        const temperature = data.main.temp;
+        const wind = data.wind.speed;
+        const humidity = data.main.humidity;
+        const icon = data.weather[0].icon;
+        $("#weatherResult").html(`
+          <h3 id="todayWeather" class="text-center">Today's Current Weather:</h3>
+          <br>
+          <h3 id="current-date" class="text-center">${cityName} : ${date}</h3>
+          <br>
+          <p class="current-temp text-center">Current Temperature: ${temperature}</p>
+          <p class="current-wind text-center">Current Wind Speed: ${wind}</p>
+          <p class="current-humidity text-center">Current Humidity: ${humidity}</p>
+          <div class="text-center">
+            <img class="justify-content-center" id="current-logo" src="http://openweathermap.org/img/wn/${icon}@2x.png">
+          </div>
+        `);
+      });
